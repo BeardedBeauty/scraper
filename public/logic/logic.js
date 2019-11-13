c376849c945069a0e714fbc662f8da2f117a22f8
John Ochs <john.w.ochs0@gmail.com>
2019-11-12 18:36:38 -0600

    fixed linking files

--- public/logic/logic.js
+++ public/logic/logic.js
@@ -1,16 +1,16 @@
-// $(document).ready(() => {
-
-// })
 const reddit = "https://www.reddit.com";
+$(document).ready(function() {
+    console.log(reddit);
+})
 
-$(document).on("click", "/scrape", (event) => {
-    event.preventDefault();
+$(document).on("click", ".neato", () => {
     let w = ["", "", ""];
     $.get("/scrape", data => {
-        for (let q = 0; q < data.length; q++) {
-            w[0] = data[q].title;
-            w[1] = reddit + data[q].link;
-            console.log(w);
-        }
+        console.log(data)
+        // for (let q = 0; q < data.length; q++) {
+        //     w[0] = data[q].title;
+        //     w[1] = reddit + data[q].link;
+        //     console.log(w);
+        // }
     });
 });
